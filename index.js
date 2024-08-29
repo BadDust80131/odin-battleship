5b06f4ae142596588e4b2d03348d26fc04b6d55c
BadDust80131 <baddust80131@gmail.com>
2024-08-28 23:32:35 -0700

    Created Ship class

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-module.exports = {};
+class Ship {
+  constructor(shipLength, timesHit, isSunk) {
+    let shipLength = shipLength;
+    let timesHit = timesHit;
+    let isSunk = isSunk;
+  }
+
+  hit() {
+    this.timesHit += 1;
+  }
+  isSunk() {
+    if (timesHit >= shipLength) {
+      this.isSunk = true;
+    }
+  }
+}
